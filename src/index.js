fd8e2ff5ce8e6339ae169c4b4f04407843365260
Iurii Kyrylenko <iurii_kyrylenko@Iuriis-MacBook-Pro.local>
2016-04-02 14:56:36 +0300

    Section 01 - An Intro to React

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyDZXPo9wUxJAcL91yqUNg9qygWOKF2BI5E';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const App = () => {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
+};
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
     <App />
-  </Provider>
-  , document.querySelector('.container'));
+  , document.querySelector('.container')
+);
